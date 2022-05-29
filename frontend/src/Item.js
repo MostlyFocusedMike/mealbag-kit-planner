85b95e2f79ce468bf41be66e05a90f76234e502f
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2022-05-29 00:57:58 -0400

    add in extra checker

--- frontend/src/Item.js
+++ frontend/src/Item.js
@@ -2,15 +2,37 @@ import { useState } from "react";
 
 const Item = ({initItem, numberOfPallets }) => {
   const [item, setItem] = useState(initItem);
-  console.log('numberOfPallets:', numberOfPallets);
-
+  const handleChange = (e) => {
+    console.log('e.target.name:', e.target.name);
+    setItem({
+      ...item,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const numberOfPacks = Math.ceil((numberOfPallets * 180 * item.itemsPerBag) / item.itemsPerPack);
+  const extraItems = (numberOfPacks * item.itemsPerPack) - (numberOfPallets * 180 * item.itemsPerBag);
   return <li>
-    <h3>{item.name} | {(numberOfPallets * 180) / item.itemsPerPack} Packs</h3>
+    <h3>{item.name} | { numberOfPacks} Packs</h3>
     <form>
       <label>Items Per Bag: </label>
-      <input type="number" value={item.itemsPerBag} />
+      <input 
+        type="number" 
+        min="1"
+        max="99"
+        value={item.itemsPerBag} 
+        name="itemsPerBag" 
+        onChange={handleChange}
+      />
       <label>Items Per Pack: </label>
-      <input type="number" value={item.itemsPerPack} />
+      <input 
+        type="number" 
+        min="1"
+        max="99"
+        value={item.itemsPerPack} 
+        name="itemsPerPack" 
+        onChange={handleChange}
+      />
+      { !!extraItems && <span>(Extra items:{extraItems})</span> }
     </form>
   </li>
 }

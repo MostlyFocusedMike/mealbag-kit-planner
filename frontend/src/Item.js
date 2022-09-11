25277fc040f51e76f399b12487b5a9aefdcb3780
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2022-09-11 01:15:02 -0400

    add css for plus minus buttons

--- frontend/src/Item.js
+++ frontend/src/Item.js
@@ -3,71 +3,81 @@ const Item = ({item, numberOfPallets, handleItemChange, idx, deleteItem, isItemI
   const handleChange = (e) => { handleItemChange(e,idx) };
   const numberOfPacks = Math.ceil((numberOfPallets * 180 * item.itemsPerBag) / item.itemsPerPack);
   const extraItems = (numberOfPacks * item.itemsPerPack) - (numberOfPallets * 180 * item.itemsPerBag);
-
+  // TODO: pretty sure the extra items math is wrong, and doesn't handle items per bag
   const { itemsPerBag, itemsPerPack } = item;
   return <li className="item">
     <h3>{item.name}</h3>
     {
       isItemInfoVisible &&
         <div className="item-info">
-          <div>
-            <label>Items Per Bag: </label>
-            <input
-              type="number"
-              min="1"
-              max="99"
-              data-idx={idx}
-              value={itemsPerBag}
-              name="itemsPerBag"
-              onChange={handleChange}
-            />
-            <button
-              type="button"
-              className='plus-minus'
-              data-val={1}
-              data-idx={idx}
-              data-name="itemsPerBag"
-              onClick={handleItemIncrement}
-            > + </button>
-            <button
-              type="button"
-              className='plus-minus'
-              data-val={-1}
-              data-idx={idx}
-              data-name="itemsPerBag"
-              onClick={handleItemIncrement}
-              > - </button>
+          <div className="bag-info">
+            <div className="item-info-label-input">
+              <label>Items Per Bag: </label>
+              <input
+                type="number"
+                min="1"
+                max="99"
+                data-idx={idx}
+                value={itemsPerBag}
+                name="itemsPerBag"
+                onChange={handleChange}
+                />
+            </div>
+            <div className="plus-minus-container">
+              <button
+                type="button"
+                className='plus-minus'
+                data-val={1}
+                data-idx={idx}
+                data-name="itemsPerBag"
+                onClick={handleItemIncrement}
+              > + </button>
+              <button
+                type="button"
+                className='plus-minus'
+                data-val={-1}
+                data-idx={idx}
+                data-name="itemsPerBag"
+                onClick={handleItemIncrement}
+                > − </button>
+            </div>
+
           </div>
-          <div>
-            <label>Items Per Pack: </label>
-            <input
-              type="number"
-              min="1"
-              max="99"
-              data-idx={idx}
-              value={itemsPerPack}
-              name="itemsPerPack"
-              onChange={handleChange}
-            />
-                        <button
-              type="button"
-              className='plus-minus'
-              data-val={1}
-              data-idx={idx}
-              data-name="itemsPerPack"
-              onClick={handleItemIncrement}
-            > + </button>
-            <button
-              type="button"
-              className='plus-minus'
-              data-val={-1}
-              data-idx={idx}
-              data-name="itemsPerPack"
-              onClick={handleItemIncrement}
-              > - </button>
-            { !!extraItems && <span> (Extra items:{extraItems})</span> }
+          <div className="pack-info">
+            <div className="item-info-label-input">
+
+              <label>Items Per Pack: </label>
+              <input
+                type="number"
+                min="1"
+                max="99"
+                data-idx={idx}
+                value={itemsPerPack}
+                name="itemsPerPack"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="plus-minus-container">
+              <button
+                type="button"
+                className='plus-minus'
+                data-val={1}
+                data-idx={idx}
+                data-name="itemsPerPack"
+                onClick={handleItemIncrement}
+              > + </button>
+              <button
+                type="button"
+                className='plus-minus'
+                data-val={-1}
+                data-idx={idx}
+                data-name="itemsPerPack"
+                onClick={handleItemIncrement}
+                > − </button>
+              </div>
           </div>
-          <button type='button' onClick={deleteItem} data-idx={idx}>Delete</button>
+          { !!extraItems && <span> (Extra items:{extraItems})</span> }
+          <button className="danger" type='button' onClick={deleteItem} data-idx={idx}>DELETE</button>
         </div>
     }
   </li>
